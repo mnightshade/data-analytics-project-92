7a41de78ad9c106b2b978e5f72330657a5cfcdfb
mnightshade <142334650+mnightshade@users.noreply.github.com>
2024-08-28 16:36:15 +0300

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -1,2 +1,35 @@
-INSERT INTO customers (customers_count) VALUES
-	 (19759);
+select COUNT(customer_id) as customers_count from customers;
+-- Данный запрос считает общее количество всех покупателей.
+
+select e.first_name ||' '|| e.last_name as seller , --Вывод имени и фамилии 
+count(s.sales_id) as operations, --Количество проведенных сделок
+floor (sum(s.quantity*p.price)) as income --Подсчет общей суммы продаж с округлением в меньшую сторону
+from sales s
+inner join employees e 
+on employee_id = sales_person_id
+inner join products p 
+on s.product_id = p.product_id 
+group by seller -- Группировка по продовцам
+order by income desc --Сортитровка по выручке в порядке убывания
+limit 10; --топ 10
+--Данный запрос находит 10 продацов с наибольшей выручкой.
+--Выводит ФИ продавцов, общее количество продаж для каждого продовца и сумарную выручку продавца за все время.
+
+with tab as (select avg(s.quantity*p.price) as total_avg
+from sales s
+inner join products p 
+on s.product_id = p.product_id ) --нахожу среднюю выручку по всем продовцам используя CTE
+select e.first_name ||' '|| e.last_name as seller ,
+floor (avg(s.quantity*p.price)) as average_income -- Вычисление средней выручки
+from sales s
+inner join employees e 
+on employee_id = sales_person_id
+inner join products p 
+on s.product_id = p.product_id
+group by seller - 
+having (select total_avg from tab) > avg(s.quantity*p.price) -- Условие по которому выводим данные, используя having и подзапрос
+order by average_income asc;
+
+--Данный запрос выводит тех продавцов и их среднюю выручку за сделку , у кого она меньше средней общей выручки за сделку по всем продовцам
+
+
