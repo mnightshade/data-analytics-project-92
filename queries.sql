7aa8af388adab0939b36c8ca93e7b987000c9e76
mnightshade <142334650+mnightshade@users.noreply.github.com>
2024-10-04 00:49:17 +0300

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -41,6 +41,38 @@ end as age_category
 from CUSTOMERS)
 select age_category, count(age_category) as age_count 
 from tab
-group by age_category; --Группируем по возрастной категории.
+group by age_category
+order by age_category; --Группируем и сортируем по возрастной категории.
 
 -- Данный запрос выводит количество человек в определенных возрастных группах.
+
+select to_char(s.sale_date,'YYYY-MM') as selling_month, -- Вывод даты в формате год-м
+count(distinct s.customer_id) as total_customers, -- считаем количество уникальных покупателей
+sum(s.quantity * p.price) as income
+from sales as s
+inner join products p 
+on s.product_id = p.product_id 
+group by selling_month 
+order by selling_month; -- группируем и сортируем по дате
+
+-- Запрос выводит количество уникальных покупателей и выручку по месяцам.
+
+with tab as (select concat( c.first_name, ' ', c.last_name) as customer, -- ФИ покупателя
+s.sale_date, 
+concat(e.first_name,' ',e.last_name) as seller, -- ФИ продавца
+s.customer_id,
+row_number () over (partition by s.customer_id order by s.sale_date) as row, -- Сортировка покупок по дате
+p.price
+from sales s
+inner join products p 
+on s.product_id = p.product_id
+inner join customers c 
+on s.customer_id = c.customer_id 
+inner join employees e 
+on s.sales_person_id = e.employee_id )
+select customer, sale_date, seller
+from tab
+where price = 0 and row = 1 -- Первая покупка акционная
+order by customer_id; -- Сортировка по id
+
+-- Запрос предоставляет информацию о покупателях, первая покупка которых была в ходе проведения акций
